e5b0acd635b415094a11bc593933b46d7c5b98a8
XinYu27 <u2102761@siswa.um.edu.my>
2022-12-29 17:29:08 +0800

    Applied Firebase

---
+++ app/src/main/java/com/example/madchef/User.java
@@ -0,0 +1,19 @@
+package com.example.madchef;
+
+public class User {
+    private String name="none";
+    private String email="none";
+    private String password="none";
+    private String birthDate="none";
+    private String phoneNum="none";
+
+    User(String email, String name, String password, String birthDate, String phoneNum){
+        super();
+
+    }
+
+    User(String email, String password){
+
+    }
+
+}
