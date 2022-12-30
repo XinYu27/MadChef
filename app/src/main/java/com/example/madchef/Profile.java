3ffc3131e07c0325f4491ffebe042760af906e5f
XinYu27 <u2102761@siswa.um.edu.my>
2022-12-30 22:58:40 +0800

    Changes to sign up and login activity

--- app/src/main/java/com/example/madchef/Profile.java
+++ app/src/main/java/com/example/madchef/Profile.java
@@ -1,6 +1,7 @@
 package com.example.madchef;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,13 @@ public class Profile extends AppCompatActivity {
 
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_profile);
+
+        TextView name = (TextView) findViewById(R.id.showName);
+        TextView birthdate = (TextView) findViewById(R.id.showBD);
+        TextView phone = (TextView) findViewById(R.id.showPhoneNum);
+        TextView email = (TextView) findViewById(R.id.showEmail);
+
+
     }
 
 }
