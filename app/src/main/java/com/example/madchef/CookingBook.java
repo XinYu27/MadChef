35b7ab7379bb7c98e77c2e80dc94598586ab3458
Masyitah Hafidz <u2000518@siswa.um.edu.my>
2023-01-05 03:11:30 +0800

    Add drawable + navigation from bottom_navbar.xml to community.xml

--- app/src/main/java/com/example/madchef/CookingBook.java
+++ app/src/main/java/com/example/madchef/CookingBook.java
@@ -2,25 +2,22 @@ package com.example.madchef;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CookingBook#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CookingBook extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +25,7 @@ public class CookingBook extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CookingBook.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static CookingBook newInstance(String param1, String param2) {
         CookingBook fragment = new CookingBook();
         Bundle args = new Bundle();
@@ -61,4 +50,17 @@ public class CookingBook extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cooking_book, container, false);
     }
-}
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        Button CB_pref = view.findViewById(R.id.CB_pref);
+        View.OnClickListener OCLpref = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                    Navigation.findNavController(view).navigate(R.id.DestCB_Preference);
+                }
+            };
+            CB_pref.setOnClickListener(OCLpref);
+        }
+    }
